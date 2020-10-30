563a353a8cadf102eb712ebbbd91e76b1aecb7ce
eretana238 <eretana238@gmail.com>
2020-10-30 19:50:35 +0000

    updated extract_frames

--- video_player.py
+++ video_player.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import cv2
-from threading import Thread, Semaphore, Lock
+import threading
 import numpy as np
 import base64
 
@@ -10,7 +10,7 @@ import base64
 # qlock.put()
 # qlock.aquire()
 # qlock.release()
-# full.release
+# full.release()
 
 # get
 # full.aquire()
@@ -19,26 +19,42 @@ import base64
 # qlock.release()
 # empty.release()
 
-semaphore = Semaphore(2)
-qlock = Lock()
+sem = threading.Semaphore(2)
+qlock = threading.Lock()
 queue = []
 
-def extract_frames(file_name, output_buffer, max_frames=9999):
-    """
-    docstring
-    """
-    pass
-
-def convert_frames():
-    """
-    docstring
-    """
+class ExtractFrames(threading.Thread):
+    def __init__(self, file_name, output_buffer, max_frames=9999):
+        """
+        Create thread
+        """
+        threading.Thread.__init__(self)
+        self.file_name = file_name
+        self.output_buffer = output_buffer
+        self.max_frames = max_frames
     
-def display_frames(parameter_list):
-    """
-    docstring
-    """
-    pass
+    def run(self):
+        """
+        Run thread
+        """
+        count = 0
+        
+        vidcap = cv2.VideoCapture(self.file_name)
+        
+        success, image = vidcap.read()
+        
+        while success and count < self.max_frames:
+            success, jpgImage = cv2.imencode('.jpg', image)
+        
+            jpgAsText = base64.b64encode(jpgImage)
+            
+            sem.acquire()
+        
+            self.output_buffer.put(image)
+        
+            sem.release()
+        
+            success,image = vidcap.read()
 
 if __name__ == "__main__":
-    
+
