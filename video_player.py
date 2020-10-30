e5e8ddd70689099d658752e13425f657ccf127b7
eretana238 <eretana238@gmail.com>
2020-10-30 22:20:03 +0000

    implemented qlock

--- video_player.py
+++ video_player.py
@@ -5,20 +5,6 @@ import threading
 import numpy as np
 import base64
 
-# put
-# empty.aquire()
-# qlock.aquire()
-# qlock.put()
-# qlock.release()
-# full.release()
-
-# get
-# full.aquire()
-# qlock.aquire()
-# qlock.put()
-# qlock.release()
-# empty.release()
-
 class ExtractFrames(threading.Thread):
     def __init__(self, file_name, max_frames=9999):
         """
@@ -28,22 +14,28 @@ class ExtractFrames(threading.Thread):
         self.file_name = file_name
         self.max_frames = max_frames
         self.count = 0
+        self.length = 0
     
     def run(self):
         """
         Run thread
         """
         vidcap = cv2.VideoCapture(self.file_name)
+        # check length
+        video_length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+        if self.max_frames < video_length:
+            self.length = self.max_frames
+        else:
+            self.length = video_length
         success, image = vidcap.read()
         while success and self.count < self.max_frames:
             success, jpgImage = cv2.imencode('.jpg', image)
             jpgAsText = base64.b64encode(jpgImage)
             # semaphore usage
             empty.acquire()
-            qlock.acquire()
-            queue.append(image)
-            print(f'Extrating frame {self.count}')
-            qlock.release()
+            with qlock:
+                queue.append(image)
+                print(f'Extrating frame {self.count}')
             full.release()
             success,image = vidcap.read()
             self.count += 1
@@ -60,19 +52,17 @@ class DisplayFrames(threading.Thread):
         """
         Run thread
         """
-        while True:
-            if len(queue) != 0:
-                # semaphore usage
-                full.acquire()
-                qlock.acquire()
+        while len(queue) > 2:
+            # semaphore usage
+            full.acquire()
+            with qlock:
                 frame = queue.pop(0)
                 print(f'Displaying frame {self.count}')
-                qlock.release()
-                empty.release()
-                cv2.imshow('Video', frame)
-                if cv2.waitKey(42) and 0xFF == ord("q"):
-                    break
-                self.count += 1
+            empty.release()
+            cv2.imshow('Video', frame)
+            if cv2.waitKey(42) and 0xFF == ord("q"):
+                break
+            self.count += 1
 
         print('Finished displaying all frames')
         # cleanup the windows
@@ -84,8 +74,10 @@ if __name__ == "__main__":
     full = threading.Semaphore(20)
     empty = threading.Semaphore(4)
     qlock = threading.Lock()
+
     queue = []
-    extract = ExtractFrames(clip, 72)
+
+    extract = ExtractFrames(clip,74)
     display = DisplayFrames()
 
     extract.start()
