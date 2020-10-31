8e5ee9f8ed03939c787c867d4ce434451c742b09
eretana238 <eretana238@gmail.com>
2020-10-31 11:51:20 +0000

    finished implementing max_frames

--- video_player.py
+++ video_player.py
@@ -4,6 +4,7 @@ import cv2
 import threading
 import numpy as np
 import base64
+import sys
 
 class ExtractFrames(threading.Thread):
     def __init__(self, file_name, max_frames=9999):
@@ -12,9 +13,8 @@ class ExtractFrames(threading.Thread):
         """
         threading.Thread.__init__(self)
         self.file_name = file_name
-        self.max_frames = max_frames
+        self.max_frames = min(video_frames, max_frames)
         self.count = 0
-        self.length = 0
     
     def run(self):
         """
@@ -23,10 +23,6 @@ class ExtractFrames(threading.Thread):
         vidcap = cv2.VideoCapture(self.file_name)
         # check length
         video_length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-        if self.max_frames < video_length:
-            self.length = self.max_frames
-        else:
-            self.length = video_length
         success, image = vidcap.read()
         while success and self.count < self.max_frames:
             success, jpgImage = cv2.imencode('.jpg', image)
@@ -47,23 +43,27 @@ class DisplayFrames(threading.Thread):
         """
         threading.Thread.__init__(self)
         self.count = 0
+        self.video_frames = video_frames
 
     def run(self):
         """
         Run thread
         """
-        while len(queue) > 2:
-            # semaphore usage
-            full.acquire()
-            with qlock:
-                frame = queue.pop(0)
-                print(f'Displaying frame {self.count}')
-            empty.release()
-            cv2.imshow('Video', frame)
-            if cv2.waitKey(42) and 0xFF == ord("q"):
+        while True:
+            if len(queue) != 0:
+                # semaphore usage
+                full.acquire()
+                with qlock:
+                    frame = queue.pop(0)
+                    print(f'Displaying frame {self.count}')
+                empty.release()
+                grayscaleFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                cv2.imshow('Video', grayscaleFrame)
+                if cv2.waitKey(42) and 0xFF == ord("q"):
+                    break
+                self.count += 1
+            if self.count == self.video_frames:
                 break
-            self.count += 1
-
         print('Finished displaying all frames')
         # cleanup the windows
         cv2.destroyAllWindows()
@@ -75,9 +75,11 @@ if __name__ == "__main__":
     empty = threading.Semaphore(4)
     qlock = threading.Lock()
 
+    video_frames = 100
+
     queue = []
 
-    extract = ExtractFrames(clip,74)
+    extract = ExtractFrames(clip)
     display = DisplayFrames()
 
     extract.start()
